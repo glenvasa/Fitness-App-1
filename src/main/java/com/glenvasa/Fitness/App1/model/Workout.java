f44beecdb067b2f42530d03df34f8d6cbcc2f7d8
glenvasa <glenvasa@gmail.com>
2022-05-08 14:28:19 -0400

    Update Workout.java

--- src/main/java/com/glenvasa/Fitness/App1/model/Workout.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/Workout.java
@@ -3,6 +3,7 @@ package com.glenvasa.Fitness.App1.model;
 import javax.persistence.*;
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Table(name = "workout")
@@ -20,15 +21,40 @@ public class Workout {
 
     private LocalTime duration;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name= "workout_exercise",
+            joinColumns = @JoinColumn(
+                    name = "workout_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name ="exercise_id", referencedColumnName = "id"
+            )
+    )
+    private Set<Exercise> exercises;
+
     public Workout() {
     }
 
-    public Workout(String workoutName, Date dateOfWorkout, LocalTime duration) {
+
+    public Workout(String workoutName, Date dateOfWorkout, LocalTime duration, User user, Set<Exercise> exercises) {
         this.workoutName = workoutName;
         this.dateOfWorkout = dateOfWorkout;
         this.duration = duration;
+        this.user = user;
+        this.exercises = exercises;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
     public Long getId() {
         return id;
     }
@@ -60,4 +86,13 @@ public class Workout {
     public void setDuration(LocalTime duration) {
         this.duration = duration;
     }
+
+    public Set<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(Set<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
 }
