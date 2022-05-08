58b30a9bd58be6cc6ac3188c5fc6e96bddfe0b8b
glenvasa <glenvasa@gmail.com>
2022-05-08 15:37:50 -0400

    Update UserService.java

--- src/main/java/com/glenvasa/Fitness/App1/service/UserService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/UserService.java
@@ -2,8 +2,11 @@ package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.UserRegistrationDto;
 import com.glenvasa.Fitness.App1.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+
+public interface UserService extends UserDetailsService { // UserDetailsService is part of Spring Security used to
+    // protect User credentials
 
     User save(UserRegistrationDto registrationDto);
 
