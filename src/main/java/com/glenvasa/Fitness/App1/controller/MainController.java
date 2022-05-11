9ee7163a44b74fb0d6b51fd1156c76c8d7509784
glenvasa <glenvasa@gmail.com>
2022-05-11 12:47:17 -0400

    created @GetMapping for /user/profile to bind logged in user object to "user" model attrib

--- src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
@@ -1,11 +1,23 @@
 package com.glenvasa.Fitness.App1.controller;
 
+import com.glenvasa.Fitness.App1.model.User;
+import com.glenvasa.Fitness.App1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
 
+    private final UserService userService;
+
+    @Autowired
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
 
 @GetMapping("/login")
     public String login() {
@@ -15,6 +27,15 @@ public class MainController {
 @GetMapping("/")
     public String home(){
     return "index";
+}
+
+@GetMapping("/user/profile")
+    public String getUserDetails(Principal principal, Model model){
+        String email = principal.getName();
+        User user = userService.loadUserByEmail(email);
+        model.addAttribute("user", user);
+        System.out.println(user.toString());
+        return "profile";
 }
 
 
