054cba4f83edd16bd9bc4b3e5c3f4db0ca176c6d
glenvasa <glenvasa@gmail.com>
2022-05-15 19:50:20 -0400

    Update ExerciseCategoryController.java

--- src/main/java/com/glenvasa/Fitness/App1/controller/ExerciseCategoryController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/ExerciseCategoryController.java
@@ -1,10 +1,8 @@
 package com.glenvasa.Fitness.App1.controller;
 
 import com.glenvasa.Fitness.App1.dto.ExerciseCategoryDto;
-import com.glenvasa.Fitness.App1.dto.UserRegistrationDto;
 import com.glenvasa.Fitness.App1.model.ExerciseCategory;
 import com.glenvasa.Fitness.App1.service.ExerciseCategoryService;
-import com.glenvasa.Fitness.App1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
