0a73c20457dbbd236201be8ac155f1b2c76f94c2
glenvasa <glenvasa@gmail.com>
2022-05-15 19:50:55 -0400

    added ExerciseCategory as 3rd parameter in constructor

--- src/main/java/com/glenvasa/Fitness/App1/dto/ExerciseCategoryDto.java
+++ src/main/java/com/glenvasa/Fitness/App1/dto/ExerciseCategoryDto.java
@@ -1,18 +1,21 @@
 package com.glenvasa.Fitness.App1.dto;
 
 
+import com.glenvasa.Fitness.App1.model.ExerciseCategory;
 
 public class ExerciseCategoryDto {
 
     private String name;
     private String description;
+    private ExerciseCategory exerciseCategory;
 
     public ExerciseCategoryDto() {
     }
 
-    public ExerciseCategoryDto(String name, String description) {
+    public ExerciseCategoryDto(String name, String description, ExerciseCategory exerciseCategory) {
         this.name = name;
         this.description = description;
+        this.exerciseCategory = exerciseCategory;
     }
 
     public String getName() {
@@ -30,4 +33,13 @@ public class ExerciseCategoryDto {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public ExerciseCategory getExerciseCategory() {
+        return exerciseCategory;
+    }
+
+    public void setExerciseCategory(ExerciseCategory exerciseCategory) {
+        this.exerciseCategory = exerciseCategory;
+    }
+
 }
