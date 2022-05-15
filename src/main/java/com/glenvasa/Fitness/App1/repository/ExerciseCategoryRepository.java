e3ae35791970bc04c149ffe0e16ce3f09da8d261
glenvasa <glenvasa@gmail.com>
2022-05-15 11:13:24 -0400

    Create ExerciseCategoryRepository.java

---
+++ src/main/java/com/glenvasa/Fitness/App1/repository/ExerciseCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.glenvasa.Fitness.App1.repository;
+
+import com.glenvasa.Fitness.App1.model.ExerciseCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExerciseCategoryRepository extends JpaRepository<ExerciseCategory, Long>{
+
+
+}
