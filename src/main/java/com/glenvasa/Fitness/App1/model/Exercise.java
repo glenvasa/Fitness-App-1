7edec0be4dc90cf9574251cc51dde1097c807d8e
glenvasa <glenvasa@gmail.com>
2022-05-15 19:51:29 -0400

    added constructors, getters and setters

--- src/main/java/com/glenvasa/Fitness/App1/model/Exercise.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/Exercise.java
@@ -17,7 +17,50 @@ public class Exercise {
     @JoinColumn(name = "exerciseCategory_id")
     private ExerciseCategory exerciseCategory;
 
+    public Exercise() {
+    }
 
+    public Exercise(Long id, String name, String description, ExerciseCategory exerciseCategory) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.exerciseCategory = exerciseCategory;
+    }
+
+    public Exercise(String name, String description, ExerciseCategory exerciseCategory) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ExerciseCategory getExerciseCategory() {
+        return exerciseCategory;
+    }
+
+    public void setExerciseCategory(ExerciseCategory exerciseCategory) {
+        this.exerciseCategory = exerciseCategory;
+    }
 
 
 }
