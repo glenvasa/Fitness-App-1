56d7c367e33f2f4076f423e1058cb5bdc900ad1d
glenvasa <glenvasa@gmail.com>
2022-05-15 19:50:28 -0400

    Create ExerciseController.java

---
+++ src/main/java/com/glenvasa/Fitness/App1/controller/ExerciseController.java
@@ -0,0 +1,53 @@
+package com.glenvasa.Fitness.App1.controller;
+
+
+import com.glenvasa.Fitness.App1.dto.ExerciseCategoryDto;
+import com.glenvasa.Fitness.App1.dto.ExerciseDto;
+import com.glenvasa.Fitness.App1.model.Exercise;
+import com.glenvasa.Fitness.App1.model.ExerciseCategory;
+import com.glenvasa.Fitness.App1.repository.ExerciseCategoryRepository;
+import com.glenvasa.Fitness.App1.service.ExerciseCategoryService;
+import com.glenvasa.Fitness.App1.service.ExerciseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/exercise")
+public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+    private final ExerciseCategoryRepository exerciseCategoryRepository;
+
+    @Autowired
+    public ExerciseController(ExerciseService exerciseService, ExerciseCategoryRepository exerciseCategoryRepository) {
+        this.exerciseService = exerciseService;
+        this.exerciseCategoryRepository = exerciseCategoryRepository;
+    }
+
+    @GetMapping()
+    public String displayExercisePage(Model model) {
+        model.addAttribute("exercise", new ExerciseDto()); // binds "exercise" attribute to the model
+
+        List<Exercise> exerciseList =  exerciseService.loadExercises();
+        model.addAttribute("exercises", exerciseList);
+
+        List<ExerciseCategory> exerciseCategoryList = exerciseCategoryRepository.findAll();
+        model.addAttribute("categories", exerciseCategoryList);
+        return "exercise";
+    }
+
+    @PostMapping
+    public String saveExercise(@ModelAttribute("exercise") ExerciseDto exerciseDto){
+        System.out.println(exerciseDto.getName() + exerciseDto.getDescription() + exerciseDto.getExerciseCategory());
+        exerciseService.save(exerciseDto);
+        return "redirect:/exercise?success";
+    }
+}
+
