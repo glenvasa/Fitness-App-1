637685ca8a1d6dd5251f973275456a75b40e1863
glenvasa <glenvasa@gmail.com>
2022-05-16 16:12:24 -0400

    Now gets ExerciseCategory category from exerciseCategoryRepository using String name received from Add Exercise form
    Passes exerciseDto.getName(), exerciseDto.getDescription() (Both from Add Exercise form) with category into new Exercise. Attempts to save this exercise to DB through exerciseRepository.save.  However, even though line 34 correctly prints all values to screen that are passed to Exercise exercise, when the object is saved on line 36 and printed to screen on line 35, it shows all fields as NULL and saves 3 NULL values in the DB.

--- src/main/java/com/glenvasa/Fitness/App1/service/ExerciseServiceImpl.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/ExerciseServiceImpl.java
@@ -2,6 +2,8 @@ package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.ExerciseDto;
 import com.glenvasa.Fitness.App1.model.Exercise;
+import com.glenvasa.Fitness.App1.model.ExerciseCategory;
+import com.glenvasa.Fitness.App1.repository.ExerciseCategoryRepository;
 import com.glenvasa.Fitness.App1.repository.ExerciseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,18 +15,31 @@ public class ExerciseServiceImpl implements ExerciseService {
 
 
     private final ExerciseRepository exerciseRepository;
+    private final ExerciseCategoryRepository exerciseCategoryRepository;
 
     @Autowired
-    public ExerciseServiceImpl(ExerciseRepository exerciseRepository) {
+    public ExerciseServiceImpl(ExerciseRepository exerciseRepository, ExerciseCategoryRepository exerciseCategoryRepository) {
         this.exerciseRepository = exerciseRepository;
+        this.exerciseCategoryRepository = exerciseCategoryRepository;
     }
 
 
     @Override
     public Exercise save(ExerciseDto exerciseDto) {
-        Exercise exercise = new Exercise(exerciseDto.getName(), exerciseDto.getDescription(), exerciseDto.getExerciseCategory());
+//        System.out.println("Ex Serv Impl" + exerciseDto.getName() + exerciseDto.getDescription() + exerciseDto.getExerciseCategory());
+          ExerciseCategory category = exerciseCategoryRepository.findByName(exerciseDto.getExerciseCategory());
+         System.out.println(category.toString()); // this Correctly prints Category object
+          Exercise exercise = new Exercise(exerciseDto.getName(), exerciseDto.getDescription(), category);
+//        Exercise exercise = new Exercise("Bicep curls", "Workout with dumbbells", new ExerciseCategory("arm exercises", "biceps, triceps, etc."));
+        System.out.println(exerciseDto.getName() + exerciseDto.getDescription() + category); // THIs correctly prints the name, description and category
+          System.out.println(exercise.toString());// BUT this shows all fields in Exercise object as Null
         return exerciseRepository.save(exercise);
     }
+
+//    @Override
+//    public Exercise save(ExerciseDto exerciseDto) {
+//        return null;
+//    }
 
     @Override
     public List<Exercise> loadExercises() {
