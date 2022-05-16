ac20156fceb0997e524c309d8c0c919628ebe241
glenvasa <glenvasa@gmail.com>
2022-05-16 07:39:06 -0400

    added @Getter/Setter/NoArgsConstructor/ToString Lombok annotations
    Will delete unnecessary code later

--- src/main/java/com/glenvasa/Fitness/App1/dto/UserRegistrationDto.java
+++ src/main/java/com/glenvasa/Fitness/App1/dto/UserRegistrationDto.java
@@ -1,9 +1,15 @@
 package com.glenvasa.Fitness.App1.dto;
 
 
+import lombok.*;
+
 import java.util.Date;
 
 // Data that user enters in Registration Page UI Form
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class UserRegistrationDto {
 
 
@@ -15,7 +21,6 @@ public class UserRegistrationDto {
         private String state;
         private String zipCode;
         private String phone1;
-        private String phone2;
         private String email;
         private String password;
         private Float height;
@@ -23,12 +28,12 @@ public class UserRegistrationDto {
         private String dateOfBirth;
 
 
-        public UserRegistrationDto() {
-        }
-
-
+//        public UserRegistrationDto() {
+//        }
+//
+//
     public UserRegistrationDto(String firstName, String lastName, String streetAddress, String city,
-                               String state, String zipCode, String phone1, String phone2, String email, String password,
+                               String state, String zipCode, String phone1, String email, String password,
                                Float height, Float weight, String dateOfBirth) {
             this.firstName = firstName;
             this.lastName = lastName;
@@ -38,7 +43,6 @@ public class UserRegistrationDto {
             this.state = state;
             this.zipCode = zipCode;
             this.phone1 = phone1;
-            this.phone2 = phone2;
             this.email = email;
             this.password = password;
             this.height = height;
@@ -46,113 +50,113 @@ public class UserRegistrationDto {
             this.dateOfBirth = dateOfBirth;
 
         }
-
-        public String getZipCode() {
-            return zipCode;
-        }
-
-        public void setZipCode(String zipCode) {
-            this.zipCode = zipCode;
-        }
-
-        public String getPhone1() {
-            return phone1;
-        }
-
-        public void setPhone1(String phone1) {
-            this.phone1 = phone1;
-        }
-
-        public String getPhone2() {
-            return phone2;
-        }
-
-        public void setPhone2(String phone2) {
-            this.phone2 = phone2;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public Float getHeight() {
-            return height;
-        }
-
-        public void setHeight(Float height) {
-            this.height = height;
-        }
-
-        public String getDateOfBirth() {
-            return dateOfBirth;
-        }
-
-        public void setDateOfBirth(String dateOfBirth) {
-            this.dateOfBirth = dateOfBirth;
-        }
-
-
-        public String getStreetAddress() {
-            return streetAddress;
-        }
-
-        public void setStreetAddress(String streetAddress) {
-            this.streetAddress = streetAddress;
-        }
-
-
-        public String getCity() {
-            return city;
-        }
-
-        public void setCity(String city) {
-            this.city = city;
-        }
-
-        public String getState() {
-            return state;
-        }
-
-        public void setState(String state) {
-            this.state = state;
-        }
-
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-    public Float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Float weight) {
-        this.weight = weight;
-    }
+//
+//        public String getZipCode() {
+//            return zipCode;
+//        }
+//
+//        public void setZipCode(String zipCode) {
+//            this.zipCode = zipCode;
+//        }
+//
+//        public String getPhone1() {
+//            return phone1;
+//        }
+//
+//        public void setPhone1(String phone1) {
+//            this.phone1 = phone1;
+//        }
+//
+//        public String getPhone2() {
+//            return phone2;
+//        }
+//
+//        public void setPhone2(String phone2) {
+//            this.phone2 = phone2;
+//        }
+//
+//        public String getEmail() {
+//            return email;
+//        }
+//
+//        public void setEmail(String email) {
+//            this.email = email;
+//        }
+//
+//        public String getPassword() {
+//            return password;
+//        }
+//
+//        public void setPassword(String password) {
+//            this.password = password;
+//        }
+//
+//        public Float getHeight() {
+//            return height;
+//        }
+//
+//        public void setHeight(Float height) {
+//            this.height = height;
+//        }
+//
+//        public String getDateOfBirth() {
+//            return dateOfBirth;
+//        }
+//
+//        public void setDateOfBirth(String dateOfBirth) {
+//            this.dateOfBirth = dateOfBirth;
+//        }
+//
+//
+//        public String getStreetAddress() {
+//            return streetAddress;
+//        }
+//
+//        public void setStreetAddress(String streetAddress) {
+//            this.streetAddress = streetAddress;
+//        }
+//
+//
+//        public String getCity() {
+//            return city;
+//        }
+//
+//        public void setCity(String city) {
+//            this.city = city;
+//        }
+//
+//        public String getState() {
+//            return state;
+//        }
+//
+//        public void setState(String state) {
+//            this.state = state;
+//        }
+//
+//
+//        public String getFirstName() {
+//            return firstName;
+//        }
+//
+//        public void setFirstName(String firstName) {
+//            this.firstName = firstName;
+//        }
+//
+//        public String getLastName() {
+//            return lastName;
+//        }
+//
+//        public void setLastName(String lastName) {
+//            this.lastName = lastName;
+//        }
+//
+//    public Float getWeight() {
+//        return weight;
+//    }
+//
+//    public void setWeight(Float weight) {
+//        this.weight = weight;
+//    }
 
 }
 
