95112a9414527da08c71cc0142a69190b915ea78
glenvasa <glenvasa@gmail.com>
2022-05-16 16:08:45 -0400

    added @Service (probably no effect here b/c not ServiceImpl)

--- src/main/java/com/glenvasa/Fitness/App1/service/ExerciseService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/ExerciseService.java
@@ -2,9 +2,11 @@ package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.ExerciseDto;
 import com.glenvasa.Fitness.App1.model.Exercise;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface ExerciseService {
     Exercise save(ExerciseDto exerciseDto);
     List<Exercise> loadExercises();
