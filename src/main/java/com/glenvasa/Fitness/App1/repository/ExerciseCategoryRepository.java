d90e45fe73592df7b59074f8e511b3adb8ebc4b3
glenvasa <glenvasa@gmail.com>
2022-05-16 16:08:21 -0400

    Added method to retrieve ExerciseCategory by String name received from Add Exercise form input

--- src/main/java/com/glenvasa/Fitness/App1/repository/ExerciseCategoryRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/ExerciseCategoryRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExerciseCategoryRepository extends JpaRepository<ExerciseCategory, Long>{
 
-
+    ExerciseCategory findByName(String s);
 }
