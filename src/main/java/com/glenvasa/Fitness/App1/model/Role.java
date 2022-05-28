422ce9d21251087c99e1545a37f9723a81b76122
glenvasa <glenvasa@gmail.com>
2022-05-27 22:12:36 -0400

    added Lombok annotations for Getters/Setters/NoArgConstructor

--- src/main/java/com/glenvasa/Fitness/App1/model/Role.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/Role.java
@@ -1,13 +1,19 @@
 package com.glenvasa.Fitness.App1.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "role")
 public class Role {
 
-    public Role() {
-    }
+
 
     public Role(String name) {
         this.name = name;
@@ -18,20 +24,6 @@ public class Role {
     private Long id;
     private String name;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
