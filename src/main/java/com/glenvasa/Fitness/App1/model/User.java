c3e0e7f6cd3a3085db444d11e1249acfcf8dd7cf
glenvasa <glenvasa@gmail.com>
2022-05-27 22:14:23 -0400

    Added OneToMany relationship with Workout (already had set ManyToOne from Workout to User) and removed Getter/Setter code and replaced with Lombok annotations

--- src/main/java/com/glenvasa/Fitness/App1/model/User.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/User.java
@@ -1,11 +1,13 @@
 package com.glenvasa.Fitness.App1.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -55,40 +57,13 @@ public class User {
     )
     private Collection<Role> roles;
 
-//    public User() {
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "id=" + id +
-//                ", firstName='" + firstName + '\'' +
-//                ", lastName='" + lastName + '\'' +
-//                ", streetAddress='" + streetAddress + '\'' +
-//                ", city='" + city + '\'' +
-//                ", state='" + state + '\'' +
-//                ", zipCode=" + zipCode +
-//                ", phone1='" + phone1 + '\'' +
-//                ", phone2='" + phone2 + '\'' +
-//                ", email='" + email + '\'' +
-//                ", password='" + password + '\'' +
-//                ", height=" + height +
-//                ", dateOfBirth=" + dateOfBirth +
-//                ", roles=" + roles +
-//                '}';
-//    }
-//
-//    public Collection<Role> getRoles() {
-//        return roles;
-//    }
-//
-//    public void setRoles(Collection<Role> roles) {
-//        this.roles = roles;
-//    }
-//
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private Set<Workout> workout = new HashSet<>();
+
    public User(String firstName, String lastName, String streetAddress, String city,
                 String state, String zipCode, String phone1, String email, String password,
-                Float height, Float weight, String dateOfBirth, Collection<Role> roles) {
+                Float height, Float weight, String dateOfBirth, Collection<Role> roles, Set<Workout> workout) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.streetAddress = streetAddress;
@@ -102,119 +77,11 @@ public class User {
         this.weight = weight;
         this.dateOfBirth = dateOfBirth;
         this.roles = roles;
+        this.workout = workout;
     }
-//
-//    public String getZipCode() {
-//        return zipCode;
-//    }
-//
-//    public void setZipCode(String zipCode) {
-//        this.zipCode = zipCode;
-//    }
-//
-//    public String getPhone1() {
-//        return phone1;
-//    }
-//
-//    public void setPhone1(String phone1) {
-//        this.phone1 = phone1;
-//    }
-//
-//    public String getPhone2() {
-//        return phone2;
-//    }
-//
-//    public void setPhone2(String phone2) {
-//        this.phone2 = phone2;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public Float getWeight() {
-//        return weight;
-//    }
-//
-//    public void setWeight(Float weight) {
-//        this.weight = weight;
-//    }
-//
-//    public Float getHeight() {
-//        return height;
-//    }
-//
-//    public void setHeight(Float height) {
-//        this.height = height;
-//    }
-//
-//    public String getDateOfBirth() {
-//        return dateOfBirth;
-//    }
-//
-//    public void setDateOfBirth(String dateOfBirth) {
-//        this.dateOfBirth = dateOfBirth;
-//    }
-//
-//
-//
-//    public String getStreetAddress() {
-//        return streetAddress;
-//    }
-//
-//    public void setStreetAddress(String streetAddress) {
-//        this.streetAddress = streetAddress;
-//    }
-//
-//    public String getCity() {
-//        return city;
-//    }
-//
-//    public void setCity(String city) {
-//        this.city = city;
-//    }
-//
-//    public String getState() {
-//        return state;
-//    }
-//
-//    public void setState(String state) {
-//        this.state = state;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
+
+
+
+
+
 }
