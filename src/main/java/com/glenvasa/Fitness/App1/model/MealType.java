4ea4c84aedba393c4e695d3240bea1e380bbc8ef
glenvasa <glenvasa@gmail.com>
2022-05-28 08:55:33 -0400

    set OneToMany relationship with Meal

--- src/main/java/com/glenvasa/Fitness/App1/model/MealType.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/MealType.java
@@ -1,11 +1,14 @@
 package com.glenvasa.Fitness.App1.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "mealType")
@@ -19,6 +22,10 @@ public class MealType {
     private Long id;
 
     String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "mealType")
+    private Set<Meal> meals = new HashSet<>();
 
     public MealType(String name){
         this.name = name;
