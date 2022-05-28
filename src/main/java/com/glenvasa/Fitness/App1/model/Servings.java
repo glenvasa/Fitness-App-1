56025bd68a375246f68ebaa53ebf87ac94b28dad
glenvasa <glenvasa@gmail.com>
2022-05-28 08:55:37 -0400

    Create Servings.java

---
+++ src/main/java/com/glenvasa/Fitness/App1/model/Servings.java
@@ -0,0 +1,39 @@
+package com.glenvasa.Fitness.App1.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "servings")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Servings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "number")
+    private Float number;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="meal_id", referencedColumnName = "id")
+    private Meal meal;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="food_id", referencedColumnName = "id")
+    private Food food;
+
+
+    public Servings(Float number, Meal meal, Food food){
+       this.number = number;
+       this.meal = meal;
+       this.food = food;
+    }
+}
+
