89e99c7eaf87816e42d4d67f3932cd288d29fe18
glenvasa <glenvasa@gmail.com>
2022-05-29 19:45:04 -0400

    Added findByName method for use in dropdown in Sets page

--- src/main/java/com/glenvasa/Fitness/App1/repository/ExerciseRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/ExerciseRepository.java
@@ -1,9 +1,11 @@
 package com.glenvasa.Fitness.App1.repository;
 
 import com.glenvasa.Fitness.App1.model.Exercise;
+import com.glenvasa.Fitness.App1.model.ExerciseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    Exercise findByName(String s);
 }
