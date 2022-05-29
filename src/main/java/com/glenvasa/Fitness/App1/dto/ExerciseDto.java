b061ab789fbab8ff576927f3009a925a8316c13f
glenvasa <glenvasa@gmail.com>
2022-05-29 18:36:24 -0400

    replaced noargconstructor, getters, setters with lombak annotations

--- src/main/java/com/glenvasa/Fitness/App1/dto/ExerciseDto.java
+++ src/main/java/com/glenvasa/Fitness/App1/dto/ExerciseDto.java
@@ -2,16 +2,20 @@ package com.glenvasa.Fitness.App1.dto;
 
 import com.glenvasa.Fitness.App1.model.Exercise;
 import com.glenvasa.Fitness.App1.model.ExerciseCategory;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
+@NoArgsConstructor
+@Getter
+@Setter
 @ToString
 public class ExerciseDto {
     private String name;
     private String description;
     private String exerciseCategory;
 
-    public ExerciseDto() {
-    }
 
     public ExerciseDto(String name, String description, String exerciseCategory) {
         this.name = name;
@@ -19,28 +23,4 @@ public class ExerciseDto {
         this.exerciseCategory = exerciseCategory;
     }
 
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getExerciseCategory() {
-        return exerciseCategory;
-    }
-
-    public void setExerciseCategory(String exerciseCategory) {
-        this.exerciseCategory = exerciseCategory;
-    }
 }
