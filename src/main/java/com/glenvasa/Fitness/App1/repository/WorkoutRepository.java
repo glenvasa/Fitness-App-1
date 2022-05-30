e9c18bc9833286710e50f28fa2be100772fd7a04
glenvasa <glenvasa@gmail.com>
2022-05-30 19:41:25 -0400

    added findTopByOrderByIdDesc() function to retrieve newest (just created) Workout so it can be attached to Sets when they are created

--- src/main/java/com/glenvasa/Fitness/App1/repository/WorkoutRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/WorkoutRepository.java
@@ -2,9 +2,16 @@ package com.glenvasa.Fitness.App1.repository;
 
 import com.glenvasa.Fitness.App1.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
 
+    Workout findTopByOrderByIdDesc();
+
+    @Modifying
+    @Query("update Workout w set w.dateOfWorkout = ?1, w.duration = ?2, w.workoutName = ?3 where w.id = ?4")
+    void updateWorkoutById(String dateOfWorkout, Float duration, String workoutName, Long id);
 }
