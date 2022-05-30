158a706de4d1c9ad15554e5c5278bdfc69dbf62c
glenvasa <glenvasa@gmail.com>
2022-05-30 19:39:26 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/controller/WorkoutController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/WorkoutController.java
@@ -3,8 +3,10 @@ package com.glenvasa.Fitness.App1.controller;
 import com.glenvasa.Fitness.App1.dto.ExerciseDto;
 import com.glenvasa.Fitness.App1.dto.UserRegistrationDto;
 import com.glenvasa.Fitness.App1.model.Workout;
+import com.glenvasa.Fitness.App1.repository.WorkoutRepository;
 import com.glenvasa.Fitness.App1.service.UserService;
 import com.glenvasa.Fitness.App1.service.WorkoutService;
+import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,20 +17,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class WorkoutController {
 
     private final WorkoutService workoutService;
+    private final WorkoutRepository workoutRepository;
 
     @Autowired
-    public WorkoutController(WorkoutService workoutService) {
+    public WorkoutController(WorkoutService workoutService, WorkoutRepository workoutRepository) {
         this.workoutService = workoutService;
+        this.workoutRepository = workoutRepository;
     }
 
     @GetMapping("/workouts")
     public String displayWorkoutsPage(Model model){
         List<Workout> workouts = workoutService.loadWorkouts();
+//        Workout workout = workoutRepository.findTopByOrderByIdDesc();
+//        System.out.println(workout.toString());
+//        List<Workout> filteredWorkouts = workouts.stream().filter(wo -> wo.getId() == workout.getId()).collect(Collectors.toList());
         model.addAttribute("workouts", workouts); // binds "workouts" attribute to the model right before page loads
 
         return "workouts";
