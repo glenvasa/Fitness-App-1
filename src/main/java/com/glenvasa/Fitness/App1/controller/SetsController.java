c3749f3133a7a2af6bcba00f9a1272280120b808
glenvasa <glenvasa@gmail.com>
2022-05-30 19:39:08 -0400

    trying to only display Sets that are tied to current workout. Not able to get Sets retrieval and filtering code correct yet

--- src/main/java/com/glenvasa/Fitness/App1/controller/SetsController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/SetsController.java
@@ -6,11 +6,14 @@ import com.glenvasa.Fitness.App1.dto.SetsDto;
 import com.glenvasa.Fitness.App1.model.Exercise;
 import com.glenvasa.Fitness.App1.model.ExerciseCategory;
 import com.glenvasa.Fitness.App1.model.Sets;
+import com.glenvasa.Fitness.App1.model.Workout;
 import com.glenvasa.Fitness.App1.repository.ExerciseCategoryRepository;
 import com.glenvasa.Fitness.App1.repository.ExerciseRepository;
 import com.glenvasa.Fitness.App1.repository.SetsRepository;
+import com.glenvasa.Fitness.App1.repository.WorkoutRepository;
 import com.glenvasa.Fitness.App1.service.ExerciseService;
 import com.glenvasa.Fitness.App1.service.SetsService;
+import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/sets")
@@ -27,25 +31,33 @@ public class SetsController {
 
     @Autowired
     private final SetsService setsService;
-
     private final SetsRepository setsRepository;
     private final ExerciseRepository exerciseRepository;
-    List<Exercise> exerciseList;
+    private final WorkoutRepository workoutRepository;
 
+    List<Exercise> exerciseList;
+    List<Sets> currentWorkoutSets;
+    List<Sets> setsList;
 
 
     @Autowired
-    public SetsController(SetsService exerciseService, SetsRepository setsRepository, ExerciseRepository exerciseRepository) {
+    public SetsController(SetsService exerciseService, SetsRepository setsRepository,
+                          ExerciseRepository exerciseRepository, WorkoutRepository workoutRepository) {
         this.setsService = exerciseService;
         this.setsRepository = setsRepository;
         this.exerciseRepository = exerciseRepository;
+        this.workoutRepository = workoutRepository;
     }
 
     @GetMapping()
-    public String displayExercisePage(Model model) {
+    public String displaySets(Model model) {
         model.addAttribute("set", new SetsDto()); // binds "sets" attribute to the model
 
-        List<Sets> setsList =  setsService.loadSets();
+        // want to only display Sets that have just been created and linked to newly created Workout
+        setsList =  setsService.loadSets();
+//        Workout currentWorkout = workoutRepository.findTopByOrderByIdDesc();
+//        List<Sets> currentWorkoutSets = setsList.stream().filter(s -> s.getWorkout().getId() == currentWorkout.getId()).collect(Collectors.toList());
+
         model.addAttribute("sets", setsList);
 
         exerciseList = exerciseRepository.findAll();
@@ -58,8 +70,13 @@ public class SetsController {
     public String saveSet(@ModelAttribute("sets") SetsDto setsDto){
 //        System.out.println("Ex Controller" + exerciseDto.getName() + exerciseDto.getDescription() + exerciseDto.getExerciseCategory());
 //          System.out.println(exerciseDto.getExerciseCategory());
-
         setsService.save(setsDto);
+        // want to only display Sets that have just been created and linked to newly created Workout
+
+        Workout currentWorkout = workoutRepository.findTopByOrderByIdDesc();
+//        List<Sets> setsList =  setsService.loadSets();
+//        currentWorkoutSets = setsList.stream().filter(s -> s.getWorkout().getId() == currentWorkout.getId()).collect(Collectors.toList());
+       setsList.forEach(s -> System.out.println(s));
         return "redirect:/sets?success";
     }
 }
