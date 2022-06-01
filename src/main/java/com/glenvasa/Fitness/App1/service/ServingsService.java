17530e0cb84d7795d7551ed89fd9ddcb7d79e47f
glenvasa <glenvasa@gmail.com>
2022-06-01 18:24:40 -0400

    Create ServingsService.java

---
+++ src/main/java/com/glenvasa/Fitness/App1/service/ServingsService.java
@@ -0,0 +1,14 @@
+package com.glenvasa.Fitness.App1.service;
+
+import com.glenvasa.Fitness.App1.dto.ServingsDto;
+import com.glenvasa.Fitness.App1.model.Servings;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ServingsService {
+    Servings save(ServingsDto servingsDto);
+    List<Servings> loadServings();
+
+}
