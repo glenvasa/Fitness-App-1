07c9aa75f979eedb4ad3bbfba68386f3189e55e2
glenvasa <glenvasa@gmail.com>
2022-06-01 08:48:55 -0400

    added @Transactional to updateWorkoutById query to make it work
    Before adding @Transactional was getting 500 error re: jpa stating I was performing an update/delete

--- src/main/java/com/glenvasa/Fitness/App1/repository/WorkoutRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/WorkoutRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
@@ -12,6 +13,9 @@ public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     Workout findTopByOrderByIdDesc();
 
     @Modifying
+    @Transactional
     @Query("update Workout w set w.dateOfWorkout = ?1, w.duration = ?2, w.workoutName = ?3 where w.id = ?4")
     void updateWorkoutById(String dateOfWorkout, Float duration, String workoutName, Long id);
+
+//    Workout updateWorkoutById(Long id, Workout updatedWorkout);
 }
