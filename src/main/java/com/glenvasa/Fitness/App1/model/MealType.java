0efe7f9447b1099b2ffb7a4826fe2651b9f38df8
glenvasa <glenvasa@gmail.com>
2022-06-01 18:24:22 -0400

    commenting out all code for now as I think I will have this as a column in Meal Table instead of own entity
    It is just one field (plus id) which is name and could only really be breakfast, lunch, dinner, snack. Probably best as column in Meal

--- src/main/java/com/glenvasa/Fitness/App1/model/MealType.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/MealType.java
@@ -1,4 +1,4 @@
-package com.glenvasa.Fitness.App1.model;
+//package com.glenvasa.Fitness.App1.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
@@ -10,24 +10,24 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity
-@Table(name = "mealType")
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-public class MealType {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    String name;
-
-    @JsonIgnore
-    @OneToMany(mappedBy = "mealType")
-    private Set<Meal> meals = new HashSet<>();
-
-    public MealType(String name){
-        this.name = name;
-    }
-}
+//@Entity
+//@Table(name = "mealType")
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//public class MealType {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    String name;
+//
+//    @JsonIgnore
+//    @OneToMany(mappedBy = "mealType")
+//    private Set<Meal> meals = new HashSet<>();
+//
+//    public MealType(String name){
+//        this.name = name;
+//    }
+//}
