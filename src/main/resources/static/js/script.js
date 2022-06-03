04c7eb228bb9f666d6bf7f17fb14175015a3d49a
glenvasa <glenvasa@gmail.com>
2022-06-03 10:59:33 -0400

    added addServingsCalories function to update total Meal Calories every time new Serving Added

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,25 @@
-const displaySaveWorkout = () => {
-    console.log("Save Workout")
-    const grid = document.getElementById('setsGrid')
-    const addForm = document.getElementById('addSetForm')
-    const saveForm = document.getElementById('saveWorkoutForm')
-    grid.style.display = 'none'
-    addForm.style.display = 'none'
-    saveForm.style.display = 'block'
+//const displaySaveWorkout = () => {
+//    console.log("Save Workout")
+//    const grid = document.getElementById('setsGrid')
+//    const addForm = document.getElementById('addSetForm')
+//    const saveForm = document.getElementById('saveWorkoutForm')
+//    grid.style.display = 'none'
+//    addForm.style.display = 'none'
+//    saveForm.style.display = 'block'
+//}
+
+window.addEventListener("load", () => {
+    addServingsCalories();
+
+})
+
+// Add up Calories for each Serving on Meal Card and set = Total Meal Calories
+const addServingsCalories = () => {
+    const servingsCals = document.querySelectorAll('#totalCals')
+    console.log(servingsCals)
+    const mealCals = document.getElementById('mealCals')
+    let totalMealCals = 0
+    servingsCals.forEach(s => totalMealCals += Number(s.innerText));
+   console.log(totalMealCals)
+   mealCals.value = totalMealCals
 }
