6ff8bbe940287bbef278845e2c9e0134c9a422c6
glenvasa <glenvasa@gmail.com>
2022-06-03 10:50:54 -0400

    added Float mealCals

--- src/main/java/com/glenvasa/Fitness/App1/dto/MealDto.java
+++ src/main/java/com/glenvasa/Fitness/App1/dto/MealDto.java
@@ -13,10 +13,12 @@ public class MealDto {
     private String date;
     private String time;
     private String mealType;
+    private Float mealCals;
 
-    public MealDto(String date, String time, String mealType) {
+    public MealDto(String date, String time, String mealType, Float mealCals) {
         this.date = date;
         this.time = time;
         this.mealType = mealType;
+        this.mealCals = mealCals;
     }
 }
