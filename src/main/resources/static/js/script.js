23345946ddff6661d4d980489f05b2897c5f054b
glenvasa <glenvasa@gmail.com>
2022-06-04 09:53:58 -0400

    added openMealModal fn to open modal/overlay when meal date clicked and then close it when "x" button or anywhere outside of modal contend clicked

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,17 +1,11 @@
-//const displaySaveWorkout = () => {
-//    console.log("Save Workout")
-//    const grid = document.getElementById('setsGrid')
-//    const addForm = document.getElementById('addSetForm')
-//    const saveForm = document.getElementById('saveWorkoutForm')
-//    grid.style.display = 'none'
-//    addForm.style.display = 'none'
-//    saveForm.style.display = 'block'
-//}
-
+// only want "load" event listener to be added and call addServingsCalories if /servings route
+if(window.location.pathname === "/servings") {
 window.addEventListener("load", () => {
+    console.log(window.location.pathname)
     addServingsCalories();
-
 })
+
+}
 
 // Add up Calories for each Serving on Meal Card and set = Total Meal Calories
 const addServingsCalories = () => {
@@ -22,4 +16,25 @@ const addServingsCalories = () => {
     servingsCals.forEach(s => totalMealCals += Number(s.innerText));
    console.log(totalMealCals)
    mealCals.value = totalMealCals
+}
+
+// opens Meal Modal when date-button clicked
+const openMealModal = () => {
+    const mealDateButtons = document.querySelectorAll('.mealDate')
+    const mealModal = document.getElementById('mealModal')
+    const closeModalButton = document.getElementById('closeModal')
+
+    mealModal.classList.remove('hidden')
+
+    closeModalButton.onclick = () => mealModal.classList.add('hidden')
+
+    // only want window onclick to function IF model is open; otherwise it will prevent other page button onclick fns
+    window.onclick = function(e) {
+          if (e.target == mealModal && !mealModal.classList.contains('hidden')) {
+            mealModal.classList.add('hidden')
+            window.onclick = null;
+          }
+    }
+
+
 }
