fc3d94f7fde9c493ad323a136d6b88ebb0c07c03
glenvasa <glenvasa@gmail.com>
2022-06-04 17:47:22 -0400

    added displayMealsSummary which is called with GET request to "/profile/meals/{date}" from JS file fetch command

--- src/main/java/com/glenvasa/Fitness/App1/controller/ProfileController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/ProfileController.java
@@ -9,9 +9,11 @@ import com.glenvasa.Fitness.App1.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -21,6 +23,8 @@ public class ProfileController {
   private final UserService userService;
   private final MealRepository mealRepository;
   private final WorkoutRepository workoutRepository;
+  private User user;
+
 
     public ProfileController(UserService userService, MealRepository mealRepository, WorkoutRepository workoutRepository){
         this.userService = userService;
@@ -31,7 +35,7 @@ public class ProfileController {
     @GetMapping("/profile")
     public String displayUserProfile(Principal principal, Model model) {
         String email = principal.getName();
-        User user = userService.loadUserByEmail(email);
+        user = userService.loadUserByEmail(email);
         model.addAttribute("user", user);
 
         List<Meal> meals = mealRepository.findAllByUserId(user.getId());
@@ -39,12 +43,23 @@ public class ProfileController {
         model.addAttribute("mealDates", mealDates);
 
         List<Workout> workouts = workoutRepository.findAllByUserId(user.getId());
-        List workoutDates = workouts.stream().map(workout -> workout.getDateOfWorkout()).collect(Collectors.toList());
-        System.out.println(workoutDates);
+        List<String> workoutDates = workouts.stream().map(Workout::getDateOfWorkout).collect(Collectors.toList());
+//        System.out.println(workoutDates);
         model.addAttribute("workoutDates", workoutDates);
 
         return "profile";
     }
 
+    @GetMapping("/profile/meals/{date}")
+    public String displayMealsSummary(@PathVariable String date, Model model) {
 
+        List<Meal> meals = mealRepository.findAllByUserId(user.getId());
+        List<Meal> dailyMeals = meals.stream().filter(meal -> Objects.equals(meal.getDate(), date)).toList();
+        model.addAttribute("dailyMeals", dailyMeals);
+
+        System.out.println("hello from Profile Controller");
+        System.out.println(date);
+        System.out.println(dailyMeals.size());
+        return "profile"; // may not need "redirect:/profile"
+    }
 }
