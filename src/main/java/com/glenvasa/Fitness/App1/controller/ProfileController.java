1fae08b1d77c4ad0249d7295d74711a8e9ba8e0b
glenvasa <glenvasa@gmail.com>
2022-06-04 21:58:27 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/controller/ProfileController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/ProfileController.java
@@ -6,6 +6,7 @@ import com.glenvasa.Fitness.App1.model.Workout;
 import com.glenvasa.Fitness.App1.repository.MealRepository;
 import com.glenvasa.Fitness.App1.repository.WorkoutRepository;
 import com.glenvasa.Fitness.App1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +24,9 @@ public class ProfileController {
   private final UserService userService;
   private final MealRepository mealRepository;
   private final WorkoutRepository workoutRepository;
-  private User user;
 
 
+    @Autowired
     public ProfileController(UserService userService, MealRepository mealRepository, WorkoutRepository workoutRepository){
         this.userService = userService;
         this.mealRepository = mealRepository;
@@ -35,7 +36,7 @@ public class ProfileController {
     @GetMapping("/profile")
     public String displayUserProfile(Principal principal, Model model) {
         String email = principal.getName();
-        user = userService.loadUserByEmail(email);
+        User user = userService.loadUserByEmail(email);
         model.addAttribute("user", user);
 
         List<Meal> meals = mealRepository.findAllByUserId(user.getId());
@@ -51,15 +52,16 @@ public class ProfileController {
     }
 
     @GetMapping("/profile/meals/{date}")
-    public String displayMealsSummary(@PathVariable String date, Model model) {
-
+    public String displayMealsSummary(@PathVariable String date, Principal principal, Model model) {
+        String email = principal.getName();
+        User user = userService.loadUserByEmail(email);
         List<Meal> meals = mealRepository.findAllByUserId(user.getId());
         List<Meal> dailyMeals = meals.stream().filter(meal -> Objects.equals(meal.getDate(), date)).toList();
         model.addAttribute("dailyMeals", dailyMeals);
 
         System.out.println("hello from Profile Controller");
         System.out.println(date);
-        System.out.println(dailyMeals.size());
-        return "profile"; // may not need "redirect:/profile"
+        System.out.println("dailyMeals" + dailyMeals);
+        return "redirect:/profile"; // may not need "redirect:/profile"
     }
 }
