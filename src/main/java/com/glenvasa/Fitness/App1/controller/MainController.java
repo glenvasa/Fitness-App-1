148046e59fb8ce56d4aab1a9702dfda26d74c8c6
glenvasa <glenvasa@gmail.com>
2022-06-07 09:38:52 -0400

    skeleton for Personal Record Table

--- src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
@@ -1,27 +1,12 @@
 package com.glenvasa.Fitness.App1.controller;
 
-import com.glenvasa.Fitness.App1.model.ExerciseCategory;
-import com.glenvasa.Fitness.App1.model.User;
-import com.glenvasa.Fitness.App1.service.ExerciseCategoryService;
-import com.glenvasa.Fitness.App1.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class MainController {
-//
-//    private final UserService userService;
-//
-//    @Autowired
-//    public MainController(UserService userService) {
-//        this.userService = userService;
-//    }
 
 @GetMapping("/login")
     public String login() {
