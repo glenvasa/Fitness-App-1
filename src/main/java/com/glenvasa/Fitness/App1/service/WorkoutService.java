b7dd7d1d34864ac4cfea425cae4b3e1642b56d64
glenvasa <glenvasa@gmail.com>
2022-06-08 15:49:51 -0400

    commented out getPersonalRecords as will likely get the data by retrieving all Sets first

--- src/main/java/com/glenvasa/Fitness/App1/service/WorkoutService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/WorkoutService.java
@@ -2,6 +2,8 @@ package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.SetsDto;
 import com.glenvasa.Fitness.App1.dto.WorkoutDto;
+
+import com.glenvasa.Fitness.App1.model.PersonalRecords;
 import com.glenvasa.Fitness.App1.model.Sets;
 import com.glenvasa.Fitness.App1.model.Workout;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.util.List;
 public interface WorkoutService {
     Workout save(Principal principal);
     void update(WorkoutDto workoutDto, Workout workout);
+//    PersonalRecords getPersonalRecords(Principal principal);
     Workout save(WorkoutDto workoutDto, Principal principal);
     List<Workout> loadWorkouts();
 }
