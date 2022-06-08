17fffb35bb67bd13809389fe0df908478a5c8a0a
glenvasa <glenvasa@gmail.com>
2022-06-08 15:47:07 -0400

    Added FindAllByUserId to retrieve all Sets for logged in user

--- src/main/java/com/glenvasa/Fitness/App1/repository/SetsRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/SetsRepository.java
@@ -1,9 +1,17 @@
 package com.glenvasa.Fitness.App1.repository;
 
 import com.glenvasa.Fitness.App1.model.Sets;
+import com.glenvasa.Fitness.App1.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SetsRepository extends JpaRepository<Sets, Long> {
+
+    @Query("from Sets s join Workout w on s.workout.id = w.id where w.user.id = ?1")
+    List<Sets> findAllByUserId(Long id);
+
 }
