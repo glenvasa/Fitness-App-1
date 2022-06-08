382e25725395d0aaa28d69ffba50645faf673ef8
glenvasa <glenvasa@gmail.com>
2022-06-08 15:46:03 -0400

    Right now retrieving all of a user's sets a printing to console on homepage
    This works, so the service -> repository query is working which is great. Goal is to group all sets by exercise and take the top weight as the PR.

--- src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
@@ -1,23 +1,50 @@
 package com.glenvasa.Fitness.App1.controller;
 
 
+
+import com.glenvasa.Fitness.App1.model.PersonalRecords;
+import com.glenvasa.Fitness.App1.model.Sets;
+import com.glenvasa.Fitness.App1.service.SetsService;
+import com.glenvasa.Fitness.App1.service.WorkoutService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 
 @Controller
 public class MainController {
+
+    SetsService setsService;
+
+    @Autowired
+    MainController(SetsService setsService){
+        this.setsService = setsService;
+    }
 
 @GetMapping("/login")
     public String login() {
     return "login";
 }
 
-@GetMapping("/")
-    public String home(){
-    return "index";
-}
 
+
+
+//@GetMapping("/")
+//    public String home(Principal principal){
+//            PersonalRecords personalRecords = workoutService.getPersonalRecords(principal);
+//            System.out.println(personalRecords);
+//            return "index";
+//}
+
+@GetMapping("/")
+    public String home(Principal principal){
+        List<Sets> allSets = setsService.loadSetsByUserId(principal);
+            System.out.println(allSets);
+            return "index";
+}
 
 
 
