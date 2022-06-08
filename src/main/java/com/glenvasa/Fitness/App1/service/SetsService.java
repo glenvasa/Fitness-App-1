009538cb0bf3f3d6bfaa51290a3135d778106665
glenvasa <glenvasa@gmail.com>
2022-06-08 15:48:54 -0400

    Added loadSetsByUserId method

--- src/main/java/com/glenvasa/Fitness/App1/service/SetsService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/SetsService.java
@@ -4,11 +4,12 @@ import com.glenvasa.Fitness.App1.dto.SetsDto;
 import com.glenvasa.Fitness.App1.model.Sets;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.List;
 
 @Service
 public interface SetsService {
     Sets save(SetsDto setsDto);
     List<Sets> loadSets();
-
+    List<Sets> loadSetsByUserId(Principal principal);
 }
