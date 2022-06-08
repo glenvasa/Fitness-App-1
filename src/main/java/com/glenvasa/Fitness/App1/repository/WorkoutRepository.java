d1625736fd4a7b2948b09712f747cd46ad0bd58b
glenvasa <glenvasa@gmail.com>
2022-06-08 15:48:35 -0400

    Commented out my initial failed attempt at a complex query to retrieve multiple pieces of data from multiple tables
    Replacing this with the FindAllSetsById method in SetsRepository and will use Collections/ methods to get this data

--- src/main/java/com/glenvasa/Fitness/App1/repository/WorkoutRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/WorkoutRepository.java
@@ -1,6 +1,8 @@
 package com.glenvasa.Fitness.App1.repository;
 
 import com.glenvasa.Fitness.App1.model.Meal;
+
+import com.glenvasa.Fitness.App1.model.PersonalRecords;
 import com.glenvasa.Fitness.App1.model.Workout;
 import org.hibernate.jdbc.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +11,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
@@ -24,7 +28,15 @@ public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     @Query("from Workout w where w.user.id = ?1")
     List<Workout> findAllByUserId(Long id);
 
+//    @Query("MAX(s.weight), e.name, from workout w, join sets s on w.id = s.workout.id, join exercise e on s.exercise.id = e.id, where w.user.id = ?1, group by e.name")
+//    PersonalRecords findPersonalRecord(Long id);
 
 
-//    Workout updateWorkoutById(Long id, Workout updatedWorkout);
-}
+//    @PersistenceContext
+//    pEntityManager entityManager;
+//
+//    public List<Object[]> customQuery(int id) {
+//        Query nativeQuery = entityManager.createNativeQuery(product_ordered).setParameter("id",id);
+//        return nativeQuery.getResultList();
+    }
+
