0071e9fd403b156caa04f811e0c6afcd51643c08
glenvasa <glenvasa@gmail.com>
2022-06-08 17:35:53 -0400

    Added functionality to create a HashMap of all Exercises and Max Weights as key,value for the user
    to displayed in a table on Homepage on load.

--- src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/MainController.java
@@ -11,7 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -42,8 +45,38 @@ public class MainController {
 @GetMapping("/")
     public String home(Principal principal){
         List<Sets> allSets = setsService.loadSetsByUserId(principal);
-            System.out.println(allSets);
-            return "index";
+//            System.out.println("The number of sets for " + principal.getName() + " is " + allSets.size());
+
+    Map<String, Float> personalRecords = new HashMap<>();
+    allSets.forEach(sets -> {
+        String exerciseName = sets.getExercise().getName();
+        Float weight = sets.getWeight();
+
+
+        //retrieves current maxWeight value for exerciseName key
+//        Float maxWeight = personalRecords.get(exerciseName); // may not need this code
+
+        // if key/exerciseName doesn't exist in Map, create it with weight as value
+        personalRecords.putIfAbsent(exerciseName, weight);
+        // if key exists, compare value with weight and replace if weight > value
+        personalRecords.computeIfPresent(exerciseName, (key,value) -> value >= weight ? value : weight);
+
+//        if(weight > maxWeight){
+//            // replace current maxWeight with weight if weight > maxWeight
+//             personalRecords.put(exerciseName, weight);
+//        }
+
+
+
+
+//        Map<String, List<Item>> items = new HashMap<>();
+//        items.computeIfAbsent(key, k -> new ArrayList<>()).add(item);
+
+    });
+        System.out.println(personalRecords);
+
+         return "index";
+
 }
 
 
