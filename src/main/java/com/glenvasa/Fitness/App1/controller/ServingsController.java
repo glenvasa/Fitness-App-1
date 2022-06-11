4d26eb4ad4703b330a042ed233299f679dd1f77f
glenvasa <glenvasa@gmail.com>
2022-06-11 16:20:04 -0400

    @PostMapping to /servings/delete/{servingId}

--- src/main/java/com/glenvasa/Fitness/App1/controller/ServingsController.java
+++ src/main/java/com/glenvasa/Fitness/App1/controller/ServingsController.java
@@ -13,10 +13,7 @@ import com.glenvasa.Fitness.App1.service.WorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -70,6 +67,12 @@ public class ServingsController {
         public String saveServing(@ModelAttribute("servings") ServingsDto servingsDto){
             servingsService.save(servingsDto);
             return "redirect:/servings?success";
+        }
+
+        @PostMapping("/servings/delete/{servingId}")
+        public String deleteServing(@PathVariable Long servingId) {
+        servingsService.deleteServing(servingId);
+        return "redirect:/servings";
         }
 
         @PostMapping("/meal/update")
