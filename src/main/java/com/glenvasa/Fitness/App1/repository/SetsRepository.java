9c0cfeed32cba5ab2d0ddf37ee7ada086f80adfd
glenvasa <glenvasa@gmail.com>
2022-06-11 11:41:40 -0400

    added overridden deleteById abstract method

--- src/main/java/com/glenvasa/Fitness/App1/repository/SetsRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/SetsRepository.java
@@ -3,8 +3,10 @@ package com.glenvasa.Fitness.App1.repository;
 import com.glenvasa.Fitness.App1.model.Sets;
 import com.glenvasa.Fitness.App1.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,5 +15,12 @@ public interface SetsRepository extends JpaRepository<Sets, Long> {
 
     @Query("from Sets s join Workout w on s.workout.id = w.id where w.user.id = ?1")
     List<Sets> findAllByUserId(Long id);
+
+    @Override
+    @Modifying
+    @Transactional
+    @Query("delete Sets s where s.id = ?1")
+    void deleteById(Long aLong);
+
 
 }
