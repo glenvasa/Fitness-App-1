616b19ad8c83378bd8db32dbaea9c87ea2bf71c6
glenvasa <glenvasa@gmail.com>
2022-06-11 16:20:34 -0400

    overridden deleteById method

--- src/main/java/com/glenvasa/Fitness/App1/repository/ServingsRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/ServingsRepository.java
@@ -2,8 +2,17 @@ package com.glenvasa.Fitness.App1.repository;
 
 import com.glenvasa.Fitness.App1.model.Servings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ServingsRepository extends JpaRepository<Servings, Long> {
+
+    @Override
+    @Modifying
+    @Transactional
+    @Query("delete Servings s where s.id = ?1")
+    void deleteById(Long aLong);
 }
