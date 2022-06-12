d38077fa63a28677bfb9de633a5845c5e35e5d32
glenvasa <glenvasa@gmail.com>
2022-06-11 22:27:57 -0400

    changed to lombok annotations

--- src/main/java/com/glenvasa/Fitness/App1/dto/ExerciseCategoryDto.java
+++ src/main/java/com/glenvasa/Fitness/App1/dto/ExerciseCategoryDto.java
@@ -2,43 +2,22 @@ package com.glenvasa.Fitness.App1.dto;
 
 
 import com.glenvasa.Fitness.App1.model.ExerciseCategory;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class ExerciseCategoryDto {
 
     private String name;
     private String description;
     private ExerciseCategory exerciseCategory;
 
-    public ExerciseCategoryDto() {
-    }
-
     public ExerciseCategoryDto(String name, String description, ExerciseCategory exerciseCategory) {
         this.name = name;
         this.description = description;
-        this.exerciseCategory = exerciseCategory;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public ExerciseCategory getExerciseCategory() {
-        return exerciseCategory;
-    }
-
-    public void setExerciseCategory(ExerciseCategory exerciseCategory) {
         this.exerciseCategory = exerciseCategory;
     }
 
