0b8f1ff27611e9818229a28478e7b448a1d6d34d
glenvasa <glenvasa@gmail.com>
2022-06-13 19:50:35 -0400

    commented out entire class - likely will delete as added maintCals to User model

--- src/main/java/com/glenvasa/Fitness/App1/model/HealthProfile.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/HealthProfile.java
@@ -1,56 +1,56 @@
 package com.glenvasa.Fitness.App1.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
-
-@Entity
-@Table(name = "healthProfile")
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-public class HealthProfile {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "weight")
-    private Float weight;
-
-    @Column(name = "maintenance_calories")
-    private Double maintenanceCalories;
-
-    @Column(name = "body_fat")
-    private Float bodyFat;
-
-    @Column(name = "steps")
-    private Integer steps;
-
-    @Column(name = "date")
-    private String date;
-
-    @Column(name = "time")
-    private String time;
-
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name="user_id", referencedColumnName = "id")
-    private User user;
-
-    public HealthProfile(String date, String time, Float weight, Double maintenanceCalories, Float bodyFat, Integer steps, User user){
-        this.date = date;
-        this.time = time;
-        this.weight = weight;
-        this.maintenanceCalories = maintenanceCalories;
-        this.bodyFat = bodyFat;
-        this.steps = steps;
-        this.user = user;
-    }
-}
-
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//@Entity
+//@Table(name = "healthProfile")
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//public class HealthProfile {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "weight")
+//    private Float weight;
+//
+//    @Column(name = "maintenance_calories")
+//    private Double maintenanceCalories;
+//
+//    @Column(name = "body_fat")
+//    private Float bodyFat;
+//
+//    @Column(name = "steps")
+//    private Integer steps;
+//
+//    @Column(name = "date")
+//    private String date;
+//
+//    @Column(name = "time")
+//    private String time;
+//
+//    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+//    @JoinColumn(name="user_id", referencedColumnName = "id")
+//    private User user;
+//
+//    public HealthProfile(String date, String time, Float weight, Double maintenanceCalories, Float bodyFat, Integer steps, User user){
+//        this.date = date;
+//        this.time = time;
+//        this.weight = weight;
+//        this.maintenanceCalories = maintenanceCalories;
+//        this.bodyFat = bodyFat;
+//        this.steps = steps;
+//        this.user = user;
+//    }
+//}
+//
