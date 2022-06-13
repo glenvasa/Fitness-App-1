7eac650e6b583cce16977af821c06b677a7686fb
glenvasa <glenvasa@gmail.com>
2022-06-13 14:47:51 -0400

    changed dateOfWorkout to LocalDate type

--- src/main/java/com/glenvasa/Fitness/App1/utilClass/PRStats.java
+++ src/main/java/com/glenvasa/Fitness/App1/utilClass/PRStats.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @ToString
@@ -13,9 +15,9 @@ public class PRStats {
 
     private Float weight;
     private Integer repetitions;
-    private String dateOfWorkout;
+    private LocalDate dateOfWorkout;
 
-    public PRStats(Float weight, Integer repetitions, String dateOfWorkout){
+    public PRStats(Float weight, Integer repetitions, LocalDate dateOfWorkout){
         this.weight = weight;
         this.repetitions = repetitions;
         this.dateOfWorkout = dateOfWorkout;
