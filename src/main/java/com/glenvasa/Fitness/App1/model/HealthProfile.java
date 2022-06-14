801c60cb415c8e52e0b92957769c806a3630df33
glenvasa <glenvasa@gmail.com>
2022-06-14 12:55:58 -0400

    uncommented HealthProfile Entity as it will be used/generated from Homepage Calorie Calc/Form;
    Fields include id, date, weight, exerciseLevel(Integer from 1-5) and maintenanceCalories

--- src/main/java/com/glenvasa/Fitness/App1/model/HealthProfile.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/HealthProfile.java
@@ -1,56 +1,50 @@
 package com.glenvasa.Fitness.App1.model;
 
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//import javax.persistence.*;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Entity
-//@Table(name = "healthProfile")
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@ToString
-//public class HealthProfile {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(name = "weight")
-//    private Float weight;
-//
-//    @Column(name = "maintenance_calories")
-//    private Double maintenanceCalories;
-//
-//    @Column(name = "body_fat")
-//    private Float bodyFat;
-//
-//    @Column(name = "steps")
-//    private Integer steps;
-//
-//    @Column(name = "date")
-//    private String date;
-//
-//    @Column(name = "time")
-//    private String time;
-//
-//    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-//    @JoinColumn(name="user_id", referencedColumnName = "id")
-//    private User user;
-//
-//    public HealthProfile(String date, String time, Float weight, Double maintenanceCalories, Float bodyFat, Integer steps, User user){
-//        this.date = date;
-//        this.time = time;
-//        this.weight = weight;
-//        this.maintenanceCalories = maintenanceCalories;
-//        this.bodyFat = bodyFat;
-//        this.steps = steps;
-//        this.user = user;
-//    }
-//}
-//
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "healthProfile")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class HealthProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "weight")
+    private Float weight;
+
+    @Column(name = "exercise_level")
+    private Integer exerciseLevel;
+
+    @Column(name = "maintenance_calories")
+    private Double maintenanceCalories;
+
+   // changed to mirror Workout and Meal ManyToOne properties.
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    private User user;
+
+    public HealthProfile(LocalDate date, Float weight, Integer exerciseLevel, Double maintenanceCalories, User user){
+        this.date = date;
+        this.weight = weight;
+        this.exerciseLevel = exerciseLevel;
+        this.maintenanceCalories = maintenanceCalories;
+        this.user = user;
+    }
+}
+
