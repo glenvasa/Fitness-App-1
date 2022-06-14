d3163e64fe79cffb871eb696cc1009dc7dbbc305
glenvasa <glenvasa@gmail.com>
2022-06-14 12:47:45 -0400

    updated updateUserById query to remove weight and change phone1 to phone; also commented out (to be deleted) updateUserMaintCals query as maintenanceCalories will be a HealthProfile field

--- src/main/java/com/glenvasa/Fitness/App1/repository/UserRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/UserRepository.java
@@ -12,18 +12,18 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email); // Spring data JPA creates a query based on method name
 
-    @Modifying
-    @Transactional
-    @Query("update User u set u.maintCals = ?1 where u.id = ?2")
-    void updateUserMaintCals(Integer maintCals, Long id);
+//    @Modifying
+//    @Transactional
+//    @Query("update User u set u.maintCals = ?1 where u.id = ?2")
+//    void updateUserMaintCals(Integer maintCals, Long id);
 
 
     @Modifying
     @Transactional
     @Query("update User u set u.firstName = ?1, u.lastName = ?2, u.streetAddress = ?3, u.city = ?4, u.state = ?5, " +
-            "u.zipCode = ?6, u.phone1 = ?7, u.height = ?8, u.weight = ?9, u.dateOfBirth = ?10 where u.id = ?11")
+            "u.zipCode = ?6, u.phone = ?7, u.height = ?8, u.dateOfBirth = ?9 where u.id = ?10")
     void updateUserById(String firstName, String lastName, String streetAddress, String city,
-                           String state, String zipCode, String phone1, Float height, Float weight, String dateOfBirth, Long id);
+                           String state, String zipCode, String phone, Float height, String dateOfBirth, Long id);
 
 
     @Override
