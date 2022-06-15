e8961e400b03e091b96069629a1a66aff65302b0
glenvasa <glenvasa@gmail.com>
2022-06-14 21:55:00 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/repository/HealthProfileRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/HealthProfileRepository.java
@@ -4,14 +4,23 @@ package com.glenvasa.Fitness.App1.repository;
 import com.glenvasa.Fitness.App1.model.Exercise;
 import com.glenvasa.Fitness.App1.model.HealthProfile;
 import com.glenvasa.Fitness.App1.model.User;
+import com.glenvasa.Fitness.App1.model.Workout;
 import com.glenvasa.Fitness.App1.service.HealthProfileService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
 @Repository
 public interface HealthProfileRepository extends JpaRepository<HealthProfile, Long> {
 
-    HealthProfile save(LocalDate date, Float weight, Integer exerciseLevel, Double maintenanceCalories, User user);
+    HealthProfile findTopByOrderByIdDesc();
+
+//    @Modifying
+//    @Transactional
+//    @Query("insert Meal m where m.id = ?1")
+//    HealthProfile save(LocalDate date, Float weight, Integer exerciseLevel, Double maintenanceCalories, User user);
 }
