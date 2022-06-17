b76cc4fc0732055af53184fc8d6bb1cc0f64d4c6
glenvasa <glenvasa@gmail.com>
2022-06-17 16:17:45 -0400

    abstract method to find User's Health Profiles created today

--- src/main/java/com/glenvasa/Fitness/App1/service/HealthProfileService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/HealthProfileService.java
@@ -6,7 +6,12 @@ import com.glenvasa.Fitness.App1.model.HealthProfile;
 import com.glenvasa.Fitness.App1.model.User;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface HealthProfileService {
     HealthProfile save(HealthProfileDto healthProfileDto, Principal principal);
+
+
+    List<HealthProfile> findTodayProfiles(Principal principal, LocalDate today);
 }
