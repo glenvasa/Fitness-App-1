31c20c6b20eb980e0c413ad4c8f6d99cfd84e5c7
glenvasa <glenvasa@gmail.com>
2022-06-16 20:37:09 -0400

    Create TwilioConfiguration.java

---
+++ src/main/java/com/glenvasa/Fitness/App1/textUser/TwilioConfiguration.java
@@ -0,0 +1,42 @@
+package com.glenvasa.Fitness.App1.textUser;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("twilio")  // this auto grabs the values for the fields from application.yml
+public class TwilioConfiguration {
+
+    private String accountSid;
+    private String authToken;
+    private String trialNumber;
+
+    public TwilioConfiguration(){
+
+    }
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    public void setAccountSid(String accountSid) {
+        this.accountSid = accountSid;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getTrialNumber() {
+        return trialNumber;
+    }
+
+    public void setTrialNumber(String trialNumber) {
+        this.trialNumber = trialNumber;
+    }
+}
