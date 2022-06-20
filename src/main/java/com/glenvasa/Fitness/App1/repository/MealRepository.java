ff07f4c0a26874b552b1a25e00fa071dbf71ff0c
glenvasa <glenvasa@gmail.com>
2022-06-20 19:53:29 -0400

    updated updateMealById Query to include mealFat, mealCarbs, and mealProtein recently added as Meal model fields

--- src/main/java/com/glenvasa/Fitness/App1/repository/MealRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/MealRepository.java
@@ -20,8 +20,8 @@ public interface MealRepository extends JpaRepository<Meal, Long> {
 
     @Modifying
     @Transactional
-    @Query("update Meal m set m.date = ?1, m.time = ?2, m.mealType = ?3, m.mealCals = ?4 where m.id = ?5")
-    void updateMealById(LocalDate date, LocalTime time, String mealType, Float mealCals, Long id);
+    @Query("update Meal m set m.date = ?1, m.time = ?2, m.mealType = ?3, m.mealCals = ?4, m.mealFat = ?5, m.mealCarbs = ?6, m.mealProtein = ?7 where m.id = ?8")
+    void updateMealById(LocalDate date, LocalTime time, String mealType, Float mealCals, Float mealFat, Float mealCarbs, Float mealProtein, Long id);
 
 
     @Query("from Meal m where m.user.id = ?1")
