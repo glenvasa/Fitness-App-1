bcb28c226aac6ed67abdb41b8e838d3415cf6895
glenvasa <glenvasa@gmail.com>
2022-06-20 12:31:09 -0400

    added 4 arg constructor (added workoutId for use in calculation in SetsController to determine if text should be sent to user. Likely won't need since I recognized I was keeping existing PR set only if it was > new Set, but it should remain when >= new Set b/c we want to track the date original PR was set

--- src/main/java/com/glenvasa/Fitness/App1/utilClass/PRStats.java
+++ src/main/java/com/glenvasa/Fitness/App1/utilClass/PRStats.java
@@ -16,10 +16,18 @@ public class PRStats {
     private Float weight;
     private Integer repetitions;
     private LocalDate dateOfWorkout;
+    private Long workoutId;
 
     public PRStats(Float weight, Integer repetitions, LocalDate dateOfWorkout){
         this.weight = weight;
         this.repetitions = repetitions;
         this.dateOfWorkout = dateOfWorkout;
+    }
+
+    public PRStats(Float weight, Integer repetitions, LocalDate dateOfWorkout, Long workoutId) {
+        this.weight = weight;
+        this.repetitions = repetitions;
+        this.dateOfWorkout = dateOfWorkout;
+        this.workoutId = workoutId;
     }
 }
