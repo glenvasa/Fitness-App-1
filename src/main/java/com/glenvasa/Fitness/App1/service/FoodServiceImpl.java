a31dfac742e6096f996fe3fd07be9526532f196d
glenvasa <glenvasa@gmail.com>
2022-06-20 21:45:26 -0400

    combining Food Category and Food name to save as Food name in Food constructor

--- src/main/java/com/glenvasa/Fitness/App1/service/FoodServiceImpl.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/FoodServiceImpl.java
@@ -2,7 +2,9 @@ package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.FoodDto;
 import com.glenvasa.Fitness.App1.model.Food;
+import com.glenvasa.Fitness.App1.model.FoodCategory;
 import com.glenvasa.Fitness.App1.model.Servings;
+import com.glenvasa.Fitness.App1.repository.FoodCategoryRepository;
 import com.glenvasa.Fitness.App1.repository.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,17 +16,22 @@ import java.util.List;
 public class FoodServiceImpl implements FoodService{
 
     private final FoodRepository foodRepository;
+    private final FoodCategoryRepository foodCategoryRepository;
 
     @Autowired
-    public FoodServiceImpl(FoodRepository foodRepository) {
+    public FoodServiceImpl(FoodRepository foodRepository, FoodCategoryRepository foodCategoryRepository) {
         this.foodRepository = foodRepository;
+        this.foodCategoryRepository = foodCategoryRepository;
     }
 
 
     @Override
     public Food save(FoodDto foodDto) {
-        Food food = new Food(foodDto.getName(), foodDto.getServingSize(), foodDto.getCalories(), foodDto.getFat(), foodDto.getCarbs(),
-                foodDto.getProtein(), new HashSet<Servings>());
+        FoodCategory foodCategory = foodCategoryRepository.findByName(foodDto.getFoodCategory());
+        String foodPlusCatName = foodDto.getFoodCategory() + "-" + foodDto.getName();
+
+        Food food = new Food(foodPlusCatName, foodDto.getServingSize(), foodDto.getCalories(), foodDto.getFat(), foodDto.getCarbs(),
+                foodDto.getProtein(), foodCategory, new HashSet<Servings>());
         return foodRepository.save(food);
     }
 
