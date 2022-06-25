25aff2d3f8cfe8d9a4377863da22594e92497722
glenvasa <glenvasa@gmail.com>
2022-06-25 15:28:50 -0400

    Create ExerciseServiceTests.java

---
+++ src/test/java/com/glenvasa/Fitness/App1/service/ExerciseServiceTests.java
@@ -0,0 +1,39 @@
+package com.glenvasa.Fitness.App1.service;
+
+import com.glenvasa.Fitness.App1.dto.ExerciseCategoryDto;
+import com.glenvasa.Fitness.App1.dto.ExerciseDto;
+import com.glenvasa.Fitness.App1.model.Exercise;
+import com.glenvasa.Fitness.App1.model.ExerciseCategory;
+import com.glenvasa.Fitness.App1.repository.ExerciseCategoryRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class ExerciseServiceTests {
+
+
+    @Autowired
+    private ExerciseServiceImpl exerciseService;
+
+
+
+    @Test // passes
+    public void testLoadExercises(){
+        ExerciseDto exerciseDto = new ExerciseDto();
+        Exercise exercise = exerciseService.save(exerciseDto); //saves a new Exercise to DB
+
+        List<Exercise> exerciseList = exerciseService.loadExercises(); //loads all Exercises
+
+        Assertions.assertThat(exerciseList.size()).isNotZero(); // we know there should be at least 1 Exercise in DB
+    }
+
+}
+
