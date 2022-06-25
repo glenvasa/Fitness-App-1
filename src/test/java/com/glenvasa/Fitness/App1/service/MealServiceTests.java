788edb1bbae373d6f44a427910b6ccc13652618d
glenvasa <glenvasa@gmail.com>
2022-06-25 15:29:00 -0400

    Create MealServiceTests.java

---
+++ src/test/java/com/glenvasa/Fitness/App1/service/MealServiceTests.java
@@ -0,0 +1,41 @@
+package com.glenvasa.Fitness.App1.service;
+
+import com.glenvasa.Fitness.App1.dto.HealthProfileDto;
+import com.glenvasa.Fitness.App1.model.HealthProfile;
+import com.glenvasa.Fitness.App1.model.Meal;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.security.Principal;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class MealServiceTests {
+
+
+ @Autowired
+ private MealServiceImpl mealService;
+
+ @Test // passes
+ public void testSaveMeal() {
+
+  Principal principal = new Principal() {
+   @Override
+   public String getName() {
+    return "Jim";
+   }
+  };
+
+
+  Meal savedMeal = mealService.save(principal);
+  Assertions.assertThat(savedMeal.getId()).isNotNull();
+ }
+
+ }
+
+
