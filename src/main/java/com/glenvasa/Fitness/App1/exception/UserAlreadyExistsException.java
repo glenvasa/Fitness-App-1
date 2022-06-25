8a985190984fbd20fd2b15a5a50392d8dd8cdc47
glenvasa <glenvasa@gmail.com>
2022-06-25 12:28:44 -0400

    Create UserAlreadyExistsException.java

---
+++ src/main/java/com/glenvasa/Fitness/App1/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.glenvasa.Fitness.App1.exception;
+
+public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String s){
+        super(s);
+    }
+}
