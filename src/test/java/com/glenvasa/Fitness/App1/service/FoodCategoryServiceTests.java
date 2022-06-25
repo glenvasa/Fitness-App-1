7cd31708bf7dcc5419ad5fe0d0076bb2866a16f0
glenvasa <glenvasa@gmail.com>
2022-06-25 15:28:52 -0400

    Create FoodCategoryServiceTests.java

---
+++ src/test/java/com/glenvasa/Fitness/App1/service/FoodCategoryServiceTests.java
@@ -0,0 +1,31 @@
+package com.glenvasa.Fitness.App1.service;
+
+import com.glenvasa.Fitness.App1.dto.ExerciseCategoryDto;
+import com.glenvasa.Fitness.App1.dto.FoodCategoryDto;
+import com.glenvasa.Fitness.App1.model.ExerciseCategory;
+import com.glenvasa.Fitness.App1.model.FoodCategory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class FoodCategoryServiceTests {
+
+    @Autowired
+    private FoodCategoryServiceImpl foodCategoryService;
+
+
+    @Test // passes
+    public void testSaveFoodCategory(){
+        FoodCategoryDto foodCategoryDto = new FoodCategoryDto();
+        FoodCategory savedFoodCategory = foodCategoryService.save(foodCategoryDto);
+
+        Assertions.assertThat(savedFoodCategory.getId()).isNotNull();
+    }
+
+}
