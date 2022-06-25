ccf96566dd178098dbf49d3c29048481ad8effeb
glenvasa <glenvasa@gmail.com>
2022-06-25 12:29:44 -0400

    User save throw UserAlreadyExistsException

--- src/main/java/com/glenvasa/Fitness/App1/service/UserService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/UserService.java
@@ -1,6 +1,7 @@
 package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.UserRegistrationDto;
+import com.glenvasa.Fitness.App1.exception.UserAlreadyExistsException;
 import com.glenvasa.Fitness.App1.model.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -10,7 +11,7 @@ import java.security.Principal;
 public interface UserService extends UserDetailsService { // UserDetailsService is part of Spring Security used to
     // protect User credentials
 
-    User save(UserRegistrationDto registrationDto);
+    User save(UserRegistrationDto registrationDto) throws UserAlreadyExistsException;
     User loadUserByEmail(String email);
 
     void update(UserRegistrationDto userRegistrationDto, Principal principal);
