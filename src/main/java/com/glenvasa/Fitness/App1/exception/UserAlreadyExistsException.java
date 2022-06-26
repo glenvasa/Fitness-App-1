b4aa63e15d0b090656fe48c7d69679b9b641f824
glenvasa <glenvasa@gmail.com>
2022-06-26 11:49:25 -0400

    commented code

--- src/main/java/com/glenvasa/Fitness/App1/exception/UserAlreadyExistsException.java
+++ src/main/java/com/glenvasa/Fitness/App1/exception/UserAlreadyExistsException.java
@@ -1,5 +1,6 @@
 package com.glenvasa.Fitness.App1.exception;
 
+// custom exception thrown if new User attempts to register with email address used by another User
 public class UserAlreadyExistsException extends Exception {
     public UserAlreadyExistsException(String s){
         super(s);
