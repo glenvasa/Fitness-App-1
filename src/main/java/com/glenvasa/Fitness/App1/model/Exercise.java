1190fcd65127161704b8e3b3f9e8ef17c10d75e1
glenvasa <glenvasa@gmail.com>
2022-06-26 11:49:34 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/model/Exercise.java
+++ src/main/java/com/glenvasa/Fitness/App1/model/Exercise.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -43,8 +42,5 @@ public class Exercise {
         this.exerciseCategory = exerciseCategory;
         this.sets = sets;
     }
-
-
-
 
 }
