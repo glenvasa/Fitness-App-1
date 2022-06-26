ef21e678313ba333553e46c22f86b4f7ff127a08
glenvasa <glenvasa@gmail.com>
2022-06-26 11:51:45 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/service/HealthProfileService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/HealthProfileService.java
@@ -1,9 +1,7 @@
 package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.HealthProfileDto;
-import com.glenvasa.Fitness.App1.dto.UserRegistrationDto;
 import com.glenvasa.Fitness.App1.model.HealthProfile;
-import com.glenvasa.Fitness.App1.model.User;
 
 import java.security.Principal;
 import java.time.LocalDate;
@@ -11,7 +9,6 @@ import java.util.List;
 
 public interface HealthProfileService {
     HealthProfile save(HealthProfileDto healthProfileDto, Principal principal);
-
 
     List<HealthProfile> findTodayProfiles(Principal principal, LocalDate today);
 }
