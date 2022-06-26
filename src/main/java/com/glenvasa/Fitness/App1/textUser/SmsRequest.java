62cbc686eff7869ea1f667b18ae94599af9ab026
glenvasa <glenvasa@gmail.com>
2022-06-26 11:52:56 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/textUser/SmsRequest.java
+++ src/main/java/com/glenvasa/Fitness/App1/textUser/SmsRequest.java
@@ -1,25 +1,15 @@
 package com.glenvasa.Fitness.App1.textUser;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SmsRequest {
 
-    //   @NotBlank  - look for comparable annotation / library jar
-    private String phoneNumber; // destination phone number
-
-    //   @NotBlank
+    // User/Destination phone number
+    private String phoneNumber;
     private String message;
-
-    // this is what client will be sending
-//    public SmsRequest(@JsonProperty("phoneNumber") String phoneNumber,
-//                      @JsonProperty("message") String message) {
-//         this.phoneNumber = phoneNumber;
-//        this.message = message;
-//    }
 
     public SmsRequest(String phoneNumber, String message) {
         this.phoneNumber = phoneNumber;
@@ -30,13 +20,6 @@ public class SmsRequest {
 
     }
 
-//    public String getPhoneNumber() {
-//        return phoneNumber;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
 
     @Override
     public String toString() {
