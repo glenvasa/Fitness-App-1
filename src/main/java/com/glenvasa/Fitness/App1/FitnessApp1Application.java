90077c4d9b970fd090a426130b01d4d7c8f78910
glenvasa <glenvasa@gmail.com>
2022-06-26 15:47:49 -0400

    added try/catch blocks w/console print out of specific error message

--- src/main/java/com/glenvasa/Fitness/App1/FitnessApp1Application.java
+++ src/main/java/com/glenvasa/Fitness/App1/FitnessApp1Application.java
@@ -1,13 +1,23 @@
 package com.glenvasa.Fitness.App1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FitnessApp1Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(FitnessApp1Application.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(FitnessApp1Application.class, args);
+		try {
+			SpringApplication.run(FitnessApp1Application.class, args);
+		} catch (Exception e){
+			System.out.println("The following error occurred when attempting to start/run the application: " + e.getMessage());
+			logger.error("Error occurred when attempting to start/run the application");
+			}
+
 	}
 
 }
