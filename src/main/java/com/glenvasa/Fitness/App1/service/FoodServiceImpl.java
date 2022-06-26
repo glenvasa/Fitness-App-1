642300c63a8c31fe4db3c8ae5be293881c72b71b
glenvasa <glenvasa@gmail.com>
2022-06-26 11:51:40 -0400

    commented code

--- src/main/java/com/glenvasa/Fitness/App1/service/FoodServiceImpl.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/FoodServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.List;
 
+// Saves/Loads Food
 @Service
 public class FoodServiceImpl implements FoodService{
 
@@ -24,7 +25,9 @@ public class FoodServiceImpl implements FoodService{
         this.foodCategoryRepository = foodCategoryRepository;
     }
 
-
+     // Saves Food by first finding Food Category using String name extracted from FoodDto, then appends
+    // category name as prefix to Food name, which allows for more filter options for User when Creating
+    // Servings for Meals.
     @Override
     public Food save(FoodDto foodDto) {
         FoodCategory foodCategory = foodCategoryRepository.findByName(foodDto.getFoodCategory());
