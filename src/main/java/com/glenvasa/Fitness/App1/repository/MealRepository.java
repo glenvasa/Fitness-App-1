a95e6daa5e31df3f82a8ce0d831bd0932d81c240
glenvasa <glenvasa@gmail.com>
2022-06-26 11:50:40 -0400

    commented code

--- src/main/java/com/glenvasa/Fitness/App1/repository/MealRepository.java
+++ src/main/java/com/glenvasa/Fitness/App1/repository/MealRepository.java
@@ -1,23 +1,24 @@
 package com.glenvasa.Fitness.App1.repository;
 
 import com.glenvasa.Fitness.App1.model.Meal;
-import com.glenvasa.Fitness.App1.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
 
+    // finds most recently created Meal in DB overall
     Meal findTopByOrderByIdDesc();
 
+    // used when Saving a Meal (updates empty Meal just created) on Servings Page after Sets created/linked to Meal
+    // Nutritional Data auto calculated and User enters additional Meal information
     @Modifying
     @Transactional
     @Query("update Meal m set m.date = ?1, m.time = ?2, m.mealType = ?3, m.mealCals = ?4, m.mealFat = ?5, m.mealCarbs = ?6, m.mealProtein = ?7 where m.id = ?8")
