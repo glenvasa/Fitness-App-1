fe7cd7ff0ed920beab6aca912eac97997d639f96
glenvasa <glenvasa@gmail.com>
2022-06-26 11:51:57 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/service/MealService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/MealService.java
@@ -1,9 +1,7 @@
 package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.dto.MealDto;
-import com.glenvasa.Fitness.App1.dto.WorkoutDto;
 import com.glenvasa.Fitness.App1.model.Meal;
-import com.glenvasa.Fitness.App1.model.Workout;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
@@ -11,10 +9,9 @@ import java.util.List;
 
 @Service
 public interface MealService {
+
     Meal save(Principal principal);
     void update(MealDto mealDto, Meal meal);
-    Meal save(MealDto mealDto, Principal principal);
     List<Meal> loadMeals();
-
     void deleteMeal(Long mealId);
 }
