8651d106d8ea46cada5c0a4ad0e5fe11448e0c23
glenvasa <glenvasa@gmail.com>
2022-06-26 11:52:38 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/service/WorkoutService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/WorkoutService.java
@@ -1,23 +1,16 @@
 package com.glenvasa.Fitness.App1.service;
 
-import com.glenvasa.Fitness.App1.dto.SetsDto;
 import com.glenvasa.Fitness.App1.dto.WorkoutDto;
-
-import com.glenvasa.Fitness.App1.model.PersonalRecords;
-import com.glenvasa.Fitness.App1.model.Sets;
 import com.glenvasa.Fitness.App1.model.Workout;
 import org.springframework.stereotype.Service;
-
 import java.security.Principal;
 import java.util.List;
 
 @Service
 public interface WorkoutService {
+
     Workout save(Principal principal);
     void update(WorkoutDto workoutDto, Workout workout);
-//    PersonalRecords getPersonalRecords(Principal principal);
-    Workout save(WorkoutDto workoutDto, Principal principal);
     List<Workout> loadWorkouts();
-
     void deleteWorkout(Long workoutId);
 }
