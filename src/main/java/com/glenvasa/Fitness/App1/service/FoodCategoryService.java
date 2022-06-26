3efe3249c82599ba01acbcae00c088a7f579465f
glenvasa <glenvasa@gmail.com>
2022-06-26 11:51:27 -0400

    mc

--- src/main/java/com/glenvasa/Fitness/App1/service/FoodCategoryService.java
+++ src/main/java/com/glenvasa/Fitness/App1/service/FoodCategoryService.java
@@ -1,8 +1,6 @@
 package com.glenvasa.Fitness.App1.service;
 
-import com.glenvasa.Fitness.App1.dto.ExerciseCategoryDto;
 import com.glenvasa.Fitness.App1.dto.FoodCategoryDto;
-import com.glenvasa.Fitness.App1.model.ExerciseCategory;
 import com.glenvasa.Fitness.App1.model.FoodCategory;
 
 import java.util.List;
