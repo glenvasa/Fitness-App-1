88e3797ed3571b51f5a2de1a74ef88d91fec30dd
glenvasa <glenvasa@gmail.com>
2022-06-27 09:08:58 -0400

    maded testSendSms a parameterized test

--- src/test/java/com/glenvasa/Fitness/App1/service/TwilioSmsSenderTests.java
+++ src/test/java/com/glenvasa/Fitness/App1/service/TwilioSmsSenderTests.java
@@ -3,6 +3,8 @@ package com.glenvasa.Fitness.App1.service;
 import com.glenvasa.Fitness.App1.textUser.SmsRequest;
 import com.glenvasa.Fitness.App1.textUser.TwilioSmsSender;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,10 +21,16 @@ public class TwilioSmsSenderTests {
     @Autowired
     private TwilioSmsSender smsSender;
 
-    @Test // test passes; catch block/fail not reached AND I actually received the text message.
-    public void testSendSms() {
+    // First 2 Tests pass; catch block/fail not reached AND first 2 Twilio verified numbers actually received the text message.
+    // Logging.info message re: texts sent (phone number + message) appear in console
+    // 3rd test only fails b/c I didn't add the number as a verified # on Twilio for my trial account;
+    // com.twilio.exception.ApiException: The number  is unverified. Trial accounts cannot send messages to unverified numbers; verify  at twilio.com/user/account/phone-numbers/verified, or purchase a Twilio number to send messages to unverified numbers.
+    // However, 3rd test did not hit catch block for having illegal arguments as SmsRequest constructor contained valid phone # ( it passed regex validation method) and message.
+    @ParameterizedTest
+    @ValueSource(strings = {"7814678117", "8572253921", "7817841958"})
+    public void testSendSms(String phoneNumber) {
         try {
-            SmsRequest smsRequest = new SmsRequest("7814678117", "Hello from SmsServiceTests.");
+            SmsRequest smsRequest = new SmsRequest(phoneNumber, "Hello from SmsServiceTests.");
             smsSender.sendSms(smsRequest);
         } catch (IllegalArgumentException e) {
             fail("Should not have thrown any exception since valid phone Number and message inputted.");
