41c86c54ba692f52344338277cb3841a9523daab
glenvasa <glenvasa@gmail.com>
2022-06-27 10:18:19 -0400

    removed unused imports and comments

--- src/test/java/com/glenvasa/Fitness/App1/service/TwilioSmsSenderTests.java
+++ src/test/java/com/glenvasa/Fitness/App1/service/TwilioSmsSenderTests.java
@@ -2,7 +2,6 @@ package com.glenvasa.Fitness.App1.service;
 
 import com.glenvasa.Fitness.App1.textUser.SmsRequest;
 import com.glenvasa.Fitness.App1.textUser.TwilioSmsSender;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
